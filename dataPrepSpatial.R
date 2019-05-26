6d78f21c05e0f9706cdbc4e95574ace662740502
KristinBarker <kristinjbarker@gmail.com>
2019-05-26 15:18:30 -0700

    remove wolf data from spatial data prep code. Prelim export to write extraction code.

--- dataPrepSpatial.R
+++ dataPrepSpatial.R
@@ -68,46 +68,14 @@
 ####   | RAW DATA |  ####
 ### ### ### ### ### ### #
  
-    
-    
-    #### Wolf, Pack, and Collar Information #### 
-    
-        # GPS collar data from areas and times of interest (from dataPrepWolf.R)
-    
-    
-        # gpsRaw <- read.csv(paste0(datDir, "\\Wolf\\wolfHistoricCleaned.csv"))
-        # wolfDatRaw <- read.csv(paste0(datDir, "\\Wolf\\CaptureAndCollarInfo\\wolf_metadata.csv"))
-        # 
-        # # all GPS collar data made spatial (WGS84 lat/longs)
-        # gpsLL <- SpatialPointsDataFrame(
-        #   data.frame("x" = as.numeric(gpsRaw$Longitude), "y" = as.numeric(gpsRaw$Latitude)),
-        #     gpsRaw, proj4string = latlong)
-        # 
-        # # all wolf collar locations from 2019 winter field study (to delineate study area)
-        # sa2019 <- read.csv("../PredationStudy/Clusters/collarLocsProcessed/locDat-20190409.csv")
-        # sa2019 <- filter(sa2019, !is.na(UTMX))
-        # 
-        # # above, spatial (UTMs) reprojected to lat-longs
-        # saUTM <- SpatialPointsDataFrame(
-        #   data.frame("x" = as.numeric(sa2019$UTMX), "y" = as.numeric(sa2019$UTMY)),
-        #     sa2019, proj4string = utm) 
-        # saLL <- spTransform(saUTM, latlong)
-        # 
-        #  
-        # # quick n dirty trimming down of available locations
-        # gpsAOI <- crop(gpsLL, extent(saLL)) # just historic wolf locs in same extent as 2019 winter wolf locs
-        # gpsAOIrast <- spTransform(gpsAOI, latlongrast) # to crop rasters without reprojecting giant file first
-        # gpsAOIutm <- spTransform(gpsAOI, utm)
 
- 
-        
         
         
     #### Canopy Cover #### 
         
         
         canRaw <- raster(paste0(datDir, "/Land/CanopyCover/NLCD/nlcd2011_usfs_conus_canopy_cartographic.img"))
-        can <- crop(canRaw, extent(gpsAOIrast))
+        can <- crop(canRaw, extent(saAEA))
  
         
         
@@ -116,9 +84,9 @@
     #### DEM ####   
         
         demRaw <- raster(paste0(datDir, "\\Land\\DEM\\SRTM_Cody_Albers.tif"))
-        elev <- crop(demRaw, extent(gpsAOIrast))
-        asp <- terrain(dem, opt='aspect')
-        rug <- terrain(dem, opt='tri')
+        elev <- crop(demRaw, extent(saAEA))
+        asp <- terrain(elev, opt='aspect')
+        rug <- terrain(elev, opt='tri')
         
         
         
@@ -126,7 +94,7 @@
         
         
         lcRaw <- raster(paste0(datDir, "//Land//LandcoverType//NLCD//", "nlcd_2011_landcover_2011_edition_2014_10_10.img"))
-        lc <- crop(lcRaw, extent(gpsAOIrast))
+        lc <- crop(lcRaw, extent(saAEA))
 
         lcLegend <- read.csv(paste0(datDir, "//Land//LandcoverType//NLCD//nlcdLegend.csv"))        
 
@@ -149,7 +117,7 @@
         
         # from owen
         motoRaw <- st_read(paste0(datDir, "/Human/Roads/roads_GYEandWY_Tiger2016_AEA.shp"))
-        moto <- st_crop(motoRaw, extent(gpsAOIrast))
+        moto <- st_crop(motoRaw, extent(saAEA))
         plot(elev); plot(moto, add = TRUE, main = "TIGER")
         
         
@@ -211,20 +179,20 @@
   
     
     
-### ### ### ### ### ### ### ### ### ### #
-####   | CLEANED & FORMATTED DATA |  ####
-### ### ### ### ### ### ### ### ### ### #
+### ### ### ### ### ### ### ### ### 
+####   | COMBINE AND EXPORT  |  ####
+### ### ### ### ### ### ### ### ### 
  
     
     
-    #### Wolf Locations ####  
+    #### stack code for storing ... stacks ####
         
-       
-        # only use GPS collar data if have enough, or you have to do separate analyses
-        wolfDat <- wolfDatRaw %>% 
-          # only consider wolves and years of data within study area
-          
+        # myRaster <- writeRaster(stk,"myStack.grd", format="raster")
         
-        # filter out data from PACKS that overlap the extent of gpsLL
+        # just the dem-based data (elev, asp, rug) for now
+        stk <- stack(elev, asp, rug)
+        names(stk@layers) <- c("elev", "asp", "rug")
+        writeRaster(stk, "../Data/Land/testStack.grd", format = "raster", overwrite = TRUE)
+  
         
   
