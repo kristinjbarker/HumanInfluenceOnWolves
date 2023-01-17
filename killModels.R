6c98dc0d54f8fc1a841a566d81e7c6e5c7586783
KristinBarker <kristinjbarker@gmail.com>
2023-01-17 10:09:37 -0800

    plots for conference presentations

--- killModels.R
+++ killModels.R
@@ -260,9 +260,76 @@ rm(wd_kjb, wd_greg)
       
       
       
+###  ####  ####  ###  ####  ####  ###  ####  ####  ###  ####  ####  ###  ####  ###  
+  
+
+      #### Conference Plots ####
       
-                  
-      save.image("prelimKillModes.RData")
+      
+        ## setup ##
+          
+          # set black theme
+          theme_black = function(base_size = 12, base_family = "") {
+            theme_grey(base_size = base_size, base_family = base_family) %+replace%
+              theme(
+                # Specify axis options
+                axis.line = element_blank(),  
+                axis.text.x = element_text(color = "white"),  
+                axis.text.y = element_text(color = "white"),  
+                axis.ticks = element_line(color = "white", size  =  0.2),  
+                axis.title.x = element_text(size = base_size*1.5, color = "white", margin = margin(0, 10, 0, 0)),  
+                axis.title.y = element_text(size = base_size*1.5, color = "white", angle = 90, margin = margin(0, 10, 0, 0)),  
+                axis.ticks.length = unit(0.3, "lines"),   
+                # Specify legend options
+                legend.background = element_rect(color = NA, fill = "black"),  
+                legend.key = element_rect(color = "white",  fill = "black"),  
+                legend.key.size = unit(1.2, "lines"),  
+                legend.key.height = NULL,  
+                legend.key.width = NULL,      
+                legend.text = element_text(color = "white"),  
+                ####legend.title = element_text(size = base_size*0.8, face = "bold", hjust = 0, color = "white"),  
+                legend.title = element_blank(),
+                legend.position = "right",  
+                legend.text.align = NULL,  
+                legend.title.align = NULL,  
+                legend.direction = "vertical",  
+                legend.box = NULL, 
+                legend.spacing.x = unit(0.3, 'cm'),
+                # Specify panel options
+                panel.background = element_rect(fill = "black", color  =  NA),  
+                panel.border = element_rect(fill = NA, color = "white"),  
+                panel.grid.major = element_line(color = "grey35"),  
+                panel.grid.minor = element_line(color = "grey20"),  
+                panel.margin = unit(0.5, "lines"),   
+                # Specify facetting options
+                strip.background = element_rect(fill = "grey30", color = "grey10"),  
+                strip.text.x = element_text(size = base_size*0.8, color = "white"),  
+                strip.text.y = element_text(size = base_size*0.8, color = "white",angle = -90),  
+                # Specify plot options
+                plot.background = element_rect(color = "black", fill = "black"),  
+                plot.title = element_text(size = base_size*2, color = "white"),  
+                plot.margin = unit(rep(1, 4), "lines")
+              )
+          }          
+               
+      
+          plotKill <- ggplot(d, aes(y = prWolf)) +
+            stat_smooth(aes(x = distStruc, col = "Buildings"), 
+                        method = "lm", formula = y ~ poly(x, 2)) +
+            stat_smooth(aes(x = distRd, y = prWolf, col = "Routes"), 
+                        method = "lm", formula = y ~ poly(x, 2)) +            
+            coord_cartesian(ylim = c(0, 0.75), xlim = c(0, 8000)) +
+            labs(y = "Pr(Kill)", x = "Distance (m)", title = "Preliminary Predation Results") +
+            theme_black() 
+          plotKill   
+          ggsave(plotKill, filename = "./Plots/kills.jpg", dpi = 600, units = "in", width = 13, height = 7)      
+      
+
+            
+      
+###  ####  ####  ###  ####  ####  ###  ####  ####  ###  ####  ####  ###  ####  ###  
+      
+      save.image("prelimKillModels.RData")
       
       
       # playtime contrasting strength of effect of each
